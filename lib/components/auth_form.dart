2a8ffc57c8910749a8b6a79256dc51f080697f6e
Luciano Palharez <luciano.palhares.ext@infracommerce.com.br>
2023-05-29 09:33:58 -0300

    variavel modo login ou cadastro no form de login

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+enum AuthMode { Signup, Login }
+
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
 
@@ -11,6 +13,8 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  AuthMode _authMode = AuthMode.Signup;
+
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
@@ -38,7 +42,16 @@ class _AuthFormState extends State<AuthForm> {
                 ),
                 keyboardType: TextInputType.emailAddress,
                 obscureText: true,
-              )
+              ), 
+              if (_authMode == AuthMode.Signup) 
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Confirmação de Senha'
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: true,
+                )
+              
             ],
           )
         ),
