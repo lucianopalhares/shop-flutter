85b33c108c9e52f609d65309919dbea89bf5402d
Luciano Palharez <luciano.palhares.ext@infracommerce.com.br>
2023-05-31 20:01:07 -0300

    tratando as variaveis retornadas no register ou auth no login do form

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,6 +5,28 @@ import 'package:http/http.dart' as http;
 import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth extends ChangeNotifier {
+
+  String? _token;
+  String? _email;
+  String? _uid;
+  DateTime? _expireDate;
+
+  bool get isAuth {
+    final isValid = _expireDate?.isAfter(DateTime.now()) ?? false;
+    return _token != null && isValid;
+  }
+
+  String? get token {
+    return isAuth ? _token : null;
+  }
+
+  String? get email {
+    return isAuth ? _email : null;
+  }
+
+  String? get uid {
+    return isAuth ? _uid : null;
+  }
   
   Future<void> _authenticate ( 
     String email, 
@@ -26,8 +48,20 @@ class Auth extends ChangeNotifier {
 
     if (body['error'] != null) {
       throw AuthException(body['error']['message']);
+    } else {
+      _token = body['idToken'];
+      _email = body['email'];
+      _uid = body['localId'];
+
+      _expireDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(body['expiresIn'])
+        )
+      );
+
+      notifyListeners();
     }
-    
+
   }
 
   Future<void> signup(String email, String password) async {    
