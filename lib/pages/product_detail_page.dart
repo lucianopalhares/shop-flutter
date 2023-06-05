424b6f18f9599b7022bd145a59cd4ba0526afde3
Luciano Palharez <luciano.palhares.ext@infracommerce.com.br>
2023-06-05 09:29:44 -0300

    iniciando CustomScrollView na pagina do produto com um AppBar proprio e para dividir o espaço rolável

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -15,41 +15,52 @@ class ProductDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.name)
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
-                tag: product.id,//um identificador unico
-                child: Image.network(
-                  product.imageUrl,
-                  fit: BoxFit.cover,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300, 
+            pinned: true, 
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.name),
+            ),
+          ), 
+          SliverList(
+            delegate: SliverChildListDelegate(
+             [
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  child: Hero(
+                    tag: product.id,//um identificador unico
+                    child: Image.network(
+                      product.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'R\$ ${product.price}',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 20
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10
-              ),
-              width: double.infinity,
-              child: Text(
-                product.description,
-                textAlign: TextAlign.center,
-              ),
+                SizedBox(height: 10),
+                Text(
+                  'R\$ ${product.price}',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10
+                  ),
+                  width: double.infinity,
+                  child: Text(
+                    product.description,
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
             )
-          ],
-        )
+          )
+        ],
       ),
     );
   }
